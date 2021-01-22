7f152df80d3a55273e302921b7c1376bca7446e5
Administrator <Administrator@DESKTOP-ISVFPRU>
2021-01-22 17:51:31 +0900

    first

---
+++ PMSpringBoard/src/main/java/com/koreait/sboard/BoardController.java
@@ -0,0 +1,12 @@
+package com.koreait.sboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+	
+	@GetMapping("/list")
+	public void list() {	
+	}
+}
